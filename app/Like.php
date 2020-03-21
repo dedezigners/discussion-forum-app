d04fa84650e04cb56f043760534ce12b9333eabb
Atif Ibrahim <dedezigners@gmail.com>
2020-03-22 04:18:11 +0500

    creating models, migrations, controllers with relations

---
+++ app/Like.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Like extends Model
+{
+    public function reply()
+    {
+        return $this->belongsTo(Reply::class);
+    }
+}
--- app/User.php
+++ app/User.php
@@ -36,4 +36,22 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
